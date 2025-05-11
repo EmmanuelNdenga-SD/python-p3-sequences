20ea7f5a6f8faf42e3865c5bfb44d756cbb75b3f
emmanue-ndenga <emmanuel.ndenga@student.moringaschool.com>
2025-05-11 21:30:59 +0300

    adds

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
-
-def print_fibonacci(length):
-    pass
+def print_fibonacci(length): # Function that takes a parameter length
+    fib_sequence = [] # an empty list 
+    a, b = 0, 1
+    for _ in range(length):
+        fib_sequence.append(a)
+        a, b = b, a + b
+    print(fib_sequence)
